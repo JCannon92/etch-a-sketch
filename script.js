d7453afb473eb62a938b364ce9a76d336c58b10c
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-20 16:46:22 +1030

    Add event listener to generate grid button
    event listener calls createGrid()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function createGrid(size) {
-    //create grid with the supplied pixel size
-    for (let i = 0; i < size; i++) {
+function createGrid(number) {
+    //create grid with the supplied number of pixels
+    for (let i = 0; i < number; i++) {
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         container.appendChild(gridSquare);
@@ -8,9 +8,22 @@ function createGrid(size) {
 }
 
 let container = document.querySelector('div.container');
-let buttonCreateGrid = document.querySelector('button#create-grid')
+let buttonCreateGrid = document.querySelector('button#create-grid');
+let inputCreateGrid = document.querySelector('input#create-grid');
 let gridSquares = document.querySelectorAll('div.grid-square');
 
+//add event listener to create grid button that generates the grid based
+//on the value in the input
+buttonCreateGrid.addEventListener('click', () => {
+    //create grid
+    createGrid(inputCreateGrid.value);
+    //reset input value
+    inputCreateGrid.value = '';
+    //set grid border
+    container.classList.add('border-black');
+})
+
+//add event listener for each square that changes to black on mouse enter
 gridSquares.forEach(square => square.addEventListener('mouseenter', (event) => {
     event.target.classList.add('background-black');
 }))

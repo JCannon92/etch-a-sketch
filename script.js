ae1358d7878f3c1d9f186ec731d8093931910a37
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-20 17:21:25 +1030

    Finalise grid and grid square configuration
    Move gridSquare event listener inside of button click event.
    Adjust grid size to be square.

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 function createGrid(number) {
-    //create grid with the supplied number of pixels
-    for (let i = 0; i < number; i++) {
+    //create grid with the supplied number of pixels on each side
+    for (let i = 0; i < (number * number); i++) {
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
+
+        //calculate height and width
+        gridSquare.style.height = container.clientHeight / number + 'px';
+        gridSquare.style.width = container.clientWidth / number + 'px';
         container.appendChild(gridSquare);
     }
 }
 
 let container = document.querySelector('div.container');
 let buttonCreateGrid = document.querySelector('button#create-grid');
-let inputCreateGrid = document.querySelector('input#create-grid');
-let gridSquares = document.querySelectorAll('div.grid-square');
 
 //add event listener to create grid button that generates the grid based
 //on the value in the input
 buttonCreateGrid.addEventListener('click', () => {
+    let inputCreateGrid = document.querySelector('input#create-grid');
     //create grid
     createGrid(inputCreateGrid.value);
+    //add event listener for each square that changes to black on mouse enter
+    let gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(square => square.addEventListener('mouseenter', (event) => {
+        event.target.classList.add('background-black');
+    }))    
     //reset input value
     inputCreateGrid.value = '';
     //set grid border
     container.classList.add('border-black');
+
+    inputCreateGrid.focus();
 })
 
-//add event listener for each square that changes to black on mouse enter
-gridSquares.forEach(square => square.addEventListener('mouseenter', (event) => {
-    event.target.classList.add('background-black');
-}))
